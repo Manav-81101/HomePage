fe79dc43510e4dc033167035d398a9e3b1a29f0d
manav-1 <manav81101@gmail.com>
2022-01-04 14:10:49 +0530

    updated  gradientDivider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import GradientDivider from "../components/GradientDivider";
 import Navbar from "../components/Navbar";
 import Section1 from "../components/Section1";
 import Section2 from "../components/Section2";
@@ -23,75 +24,45 @@ const IndexPage = () => {
     <main>
       <Navbar />
       <Section1 />
-      <GradientContainer>
-        <h3
-          style={{
-            fontFamily: "Poppins",
-            color: "#000",
-            fontWeight: "400",
-            textAlign: "center",
-          }}
-        >
-          Are you a business owner, marketer, content writer or a designer? Join
+      <GradientDivider
+        text="Are you a business owner, marketer, content writer or a designer? Join
           the new revolution in
-          <br /> design where humans and AI interact to produce stunning
-          creatives.
-        </h3>
-      </GradientContainer>
+          design where humans and AI interact to produce stunning
+          creatives"
+      />
       <Section2 />
-      <GradientContainer style={{ position: "relative" }}>
-        <h3
-          style={{
-            fontFamily: "Poppins",
-            color: "#000",
-            fontWeight: "400",
-            textAlign: "center",
-          }}
-        >
-          In a world full of distraction, grabbing attention is key.
-          <br /> A great way to do so is by creating visuals that stand out.
-        </h3>
-        <CircleText />
-        <CircleIcon
-          style={{
-            backgroundColor: "#1882D0AA",
-            left: "calc(50% - 116.7px/2 + 49.65px)",
-            flexDirection: "column",
-            "&:after": "",
-          }}
-        >
-          <Icon src={item.src} />
-        </CircleIcon>
-      </GradientContainer>
+      <GradientDivider
+        text="In a world full of distraction, grabbing attention is key.
+           A great way to do so is by creating visuals that stand out."
+        icon={
+          <>
+            <CircleText />
+            <CircleIcon
+              style={{
+                backgroundColor: "#1882D0AA",
+                left: "calc(50% - 116.7px/2 + 49.65px)",
+                flexDirection: "column",
+                "&:after": "",
+              }}
+            >
+              <Icon src={item.src} />
+            </CircleIcon>
+          </>
+        }
+      />
       <Section3 />
-      <GradientContainer>
-        <h3
-          style={{
-            fontFamily: "Poppins",
-            color: "#000",
-            fontWeight: "400",
-            textAlign: "center",
-          }}
-        >
-          No design skills? No problem.
-          <br /> You can now create beautiful designs without having to be a
-          designer.
-        </h3>
-      </GradientContainer>
+      <GradientDivider
+        text="No design skills? No problem.
+ You can now create beautiful designs without having to be a
+          designer."
+      />
+
       <Section4 />
-      <GradientContainer>
-        <h3
-          style={{
-            fontFamily: "Poppins",
-            color: "#000",
-            fontWeight: "400",
-            textAlign: "center",
-          }}
-        >
+      <GradientDivider
+        text="
           No more sifting through design templates.
-          <br /> Sivi generates unique and stunning graphics for your content.
-        </h3>
-      </GradientContainer>
+          Sivi generates unique and stunning graphics for your content."
+      />
       <Section5 />
     </main>
   );
